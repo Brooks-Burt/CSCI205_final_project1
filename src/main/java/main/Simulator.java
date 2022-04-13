4d47d3378e5d6ee95076ec65a41dcaf1f43fc317
Connor Vucovich <cjv012@bucknell.edu>
2022-04-13 10:24:30 -0400

    "Class Skeleton Completed"

---
+++ src/main/java/main/Simulator.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Spring2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Connor Vucovich
+ * Section: 01 - 10a
+ * Date: 4/13/22
+ * Time: 10:23 AM
+ *
+ * Project: csci205_final_project
+ * Package: main
+ * Class: Simulator
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class Simulator {
+}
---
+++ src/main/java/main/World.java
@@ -0,0 +1,22 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Spring2022
+ * Instructor: Prof. Brian King
+ *
+ * Name: Connor Vucovich
+ * Section: 01 - 10a
+ * Date: 4/13/22
+ * Time: 10:21 AM
+ *
+ * Project: csci205_final_project
+ * Package: main
+ * Class: World
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package main;
+
+public class World {
+}
